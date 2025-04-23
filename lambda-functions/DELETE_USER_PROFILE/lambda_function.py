b85f7e0d7df9c72a838f1563d05f2593278aab92
Satya Kamala Immidisetty <truelotus139@gmail.com>
2025-04-23 00:29:49 -0400

    delete user

--- lambda-functions/DELETE_USER_PROFILE/lambda_function.py
+++ lambda-functions/DELETE_USER_PROFILE/lambda_function.py
@@ -1,11 +1,9 @@
-import boto3
 import json
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('Users')
+from utils import get_table
 
 def lambda_handler(event, context):
     user_id = event['pathParameters']['id']
+    table = get_table()
 
     table.delete_item(Key={'user_id': user_id})
 
