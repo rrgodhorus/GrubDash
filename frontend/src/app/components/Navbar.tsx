cc1cd562fc3151a9c1f94207cc437477d65f1b1e
Rajath Reghunath <rajathreghunath@gmail.com>
2025-05-02 01:23:49 -0400

    user name and role load

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { useAuth } from "react-oidc-context";
 import LoginButton from "./LoginButton";
 import Link from 'next/link';
 
 export default function Navbar() {
+  const auth = useAuth();
+  const name = auth.user?.profile.name;
+  // const role = auth.user?.profile["custom:user_role"];
+
   return (
     <header className="flex justify-between items-center px-4 py-4 bg-white shadow-md w-full fixed top-0 left-0">
       <h1 className="text-2xl font-bold text-orange-600">GrubDash</h1>
@@ -11,8 +18,9 @@ export default function Navbar() {
         <a href="#" className="text-gray-700 hover:text-orange-600">About Us</a>
         <a href="#" className="text-gray-700 hover:text-orange-600">Help</a>
       </nav>
-      <div className="flex space-x-4">
+      <div className="flex items-center space-x-4">
         <button className="text-gray-700 hover:text-orange-600">Cart</button>
+        <span className="self-center text-orange-600 font-semibold text-base">{name}</span>
         <LoginButton />
       </div>
     </header>
