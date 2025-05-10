3a20fc43f0a03b3b009668dd0b7abb9372c7acbe
Karthik Sunkari <karthiksunkari@Karthiks-Air.lan>
2025-05-10 12:57:36 -0400

    updated next config ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   reactStrictMode: false,
